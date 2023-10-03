1e2901efb92c0975a18dffd5d486b845fbe1d8b7
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-10-02 20:15:39 -0600

    Update BLists.java

--- src/BLists.java
+++ src/BLists.java
@@ -281,4 +281,212 @@ public class BLists {
                 .map(Map.Entry::getKey)
                 .orElse(null);
     }
+
+    private static boolean hasCycleUtil(String node, Map<String, List<String>> graph,
+                                        Set<String> visited, Set<String> recStack) {
+        if (!visited.contains(node)) {
+            visited.add(node);
+            recStack.add(node);
+
+            List<String> neighbors = graph.getOrDefault(node, Collections.emptyList());
+            for (String neighbor : neighbors) {
+                if (!visited.contains(neighbor) && hasCycleUtil(neighbor, graph, visited, recStack)) {
+                    return true;
+                } else if (recStack.contains(neighbor)) {
+                    return true;
+                }
+            }
+        }
+
+        recStack.remove(node);
+        return false;
+    }
+
+    public static boolean hasCycle(List<Pair<String, String>> edges) {
+        Map<String, List<String>> graph = edges.stream()
+                .collect(Collectors.groupingBy(Pair::first,
+                        Collectors.mapping(Pair::second, Collectors.toList())));
+
+        Set<String> visited = new HashSet<>();
+        Set<String> recStack = new HashSet<>();
+
+        for (String node : graph.keySet()) {
+            if (hasCycleUtil(node, graph, visited, recStack)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static double jaccardSimilarity(List<String> list1, List<String> list2) {
+        Set<String> set1 = new HashSet<>(list1);
+        Set<String> set2 = new HashSet<>(list2);
+
+        Set<String> union = new HashSet<>(set1);
+        union.addAll(set2);
+
+        Set<String> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+
+        return (double) intersection.size() / union.size();
+    }
+
+    public static String mostFrequentNGram(List<String> words, int n) {
+        return words.stream()
+                .flatMap(word -> IntStream.range(0, word.length() - n + 1)
+                        .mapToObj(i -> word.substring(i, i + n)))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null);
+    }
+
+    public static double median(List<Integer> numbers) {
+        List<Integer> sortedList = numbers.stream()
+                .sorted()
+                .toList();
+
+        int size = sortedList.size();
+        if (size % 2 == 0) {
+            int mid1 = sortedList.get(size / 2 - 1);
+            int mid2 = sortedList.get(size / 2);
+            return (double) (mid1 + mid2) / 2;
+        } else {
+            return sortedList.get(size / 2);
+        }
+    }
+
+    public static String longestCommonSubsequence(String s1, String s2) {
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        int i = s1.length();
+        int j = s2.length();
+        StringBuilder lcs = new StringBuilder();
+
+        while (i > 0 && j > 0) {
+            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                lcs.insert(0, s1.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+
+        return lcs.toString();
+    }
+
+    public static int[][] matrixMultiply(int[][] matrix1, int[][] matrix2) {
+        int n = matrix1.length;
+        int[][] result = new int[n][n];
+
+        IntStream.range(0, n).parallel().forEach(i ->
+                IntStream.range(0, n).forEach(j ->
+                        result[i][j] = IntStream.range(0, n)
+                                .map(k -> matrix1[i][k] * matrix2[k][j])
+                                .sum()
+                )
+        );
+
+        return result;
+    }
+
+    private static <T extends Comparable<T>> List<T> merge(List<T> left, List<T> right) {
+        List<T> result = new ArrayList<>();
+        int i = 0, j = 0;
+
+        while (i < left.size() && j < right.size()) {
+            if (left.get(i).compareTo(right.get(j)) < 0) {
+                result.add(left.get(i++));
+            } else {
+                result.add(right.get(j++));
+            }
+        }
+
+        result.addAll(left.subList(i, left.size()));
+        result.addAll(right.subList(j, right.size()));
+
+        return result;
+    }
+
+    public static <T extends Comparable<T>> List<T> parallelMergeSort(List<T> list) {
+        if (list.size() <= 1) {
+            return list;
+        }
+
+        int mid = list.size() / 2;
+        List<T> left = list.subList(0, mid);
+        List<T> right = list.subList(mid, list.size());
+
+        List<T> sortedLeft = parallelMergeSort(left);
+        List<T> sortedRight = parallelMergeSort(right);
+
+        return merge(sortedLeft, sortedRight);
+    }
+
+    public static <T> List<T> kMostFrequentElements(List<T> list, int k) {
+        Map<T, Long> frequencyMap = list.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        return frequencyMap.entrySet().stream()
+                .sorted(Map.Entry.<T, Long>comparingByValue().reversed())
+                .limit(k)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Double> movingAverage(List<Double> numbers, int windowSize) {
+        if (windowSize <= 0 || windowSize > numbers.size()) {
+            throw new IllegalArgumentException("Invalid window size");
+        }
+
+        List<Double> averages = new ArrayList<>();
+        double sum = 0;
+
+        for (int i = 0; i < windowSize; i++) {
+            sum += numbers.get(i);
+        }
+
+        averages.add(sum / windowSize);
+
+        for (int i = windowSize; i < numbers.size(); i++) {
+            sum += numbers.get(i) - numbers.get(i - windowSize);
+            averages.add(sum / windowSize);
+        }
+
+        return averages;
+    }
+
+    public static <T, R> List<R> filterAndTransform(List<T> list, Predicate<T> filter, Function<T, R> transformer) {
+        return list.stream()
+                .filter(filter)
+                .map(transformer)
+                .collect(Collectors.toList());
+    }
+
+    public static <T, K> Map<K, List<T>> groupBy(List<T> list, Function<T, K> classifier) {
+        return list.stream()
+                .collect(Collectors.groupingBy(classifier));
+    }
+
+    public static <T> List<T> topKElements(List<T> list, int k, Comparator<T> comparator) {
+        return list.stream()
+                .sorted(comparator.reversed())
+                .limit(k)
+                .collect(Collectors.toList());
+    }
 }
